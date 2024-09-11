dae2c8b468459db83f67ada72e58ab8d21e6dc8b
qayiya tshotsho <mr.qtshotsho@gmail.com>
2024-09-11 19:12:01 +0200

    Django relationship_app

--- django-models/relationship_app/query_samples.py
+++ django-models/relationship_app/query_samples.py
@@ -5,7 +5,9 @@ from .models import Library, Author
 library_name = ""
 author_name = ""
 author = ""
+library = ""
 books_by_author = Author.objects.get(name=author_name).objects.filter(author=author)
 
 books = Library.objects.get(name=library_name).books.all()
-librarian = Librarian.objects.filter(library='idk')
+
+librarian = Librarian.objects.get(library=library)
