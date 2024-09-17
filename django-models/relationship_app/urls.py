19d7b3affa7d06aa179c385c42dd26a7ff577297
qayiya tshotsho <mr.qtshotsho@gmail.com>
2024-09-17 10:41:41 +0200

    relationship_app config

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('relationship_app/', LibraryDetailView.as_view(), name='library_detail'),
     path('relationship_app/', auth_views.LogoutView.as_view(template_name='registration_app/logout.html'), name='logout'),
     path('relationship_app/', auth_views.LoginView.as_view(template_name='registration_app/login.html'), name='login'),
+    path('relationship_app/', register, name='register'),
 ]
