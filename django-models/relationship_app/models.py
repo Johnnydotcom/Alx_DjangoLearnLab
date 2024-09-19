4ea371a2d5e1e46fbabbdc63391824d12346af44
qayiya tshotsho <mr.qtshotsho@gmail.com>
2024-09-19 20:44:05 +0200

    User restrictions fix

--- django-models/relationship_app/models.py
+++ django-models/relationship_app/models.py
@@ -21,3 +21,22 @@ class Librarian(models.Model):
     name = models.CharField(max_length=100)
     library = models.OneToOneField(Library, on_delete=models.CASCADE)
     
+class Admin(models.Model):
+    name = models.CharField(max_length=100)
+    admin = models.OneToOneField(Admin, on_delete=models.CASCADE)
+    
+class Member(models.Model):
+    name = models.CharField(max_length=100)
+    member = models.OneToOneField(Member, on_delete=models.CASCADE)
+# class UserProfile(models.Model):
+#     username = models.CharField(max_length=50)
+    
+#       def __str__(self):
+#         return self.username
+    
+# class Profile(models.Model):
+#     user = models.OneToOneField(User, on_delete=models.CASCADE)
+#     role = models.CharField(max_length=50)
+    
+#     def __str__(self):
+# return f"{self.user.username} - {self.role}"
