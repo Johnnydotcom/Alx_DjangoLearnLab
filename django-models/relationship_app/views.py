8836eebb4d5f7d1f869cc8de193c59687cd413ac
qayiya tshotsho <mr.qtshotsho@gmail.com>
2024-09-20 19:42:30 +0200

    User restrictions fix

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -6,7 +6,7 @@ from django.views.generic.detail import DetailView
 from django.views.generic import DetailView
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.decorators import user_passes_test
+from django.contrib.auth.decorators import user_passes_test, login_required
 
 
 
@@ -31,3 +31,26 @@ def register(request):
     else:
         form = UserCreationForm()
     return render(request, 'registration_app/register.html', {'form': form})
+
+def is_admin(user):
+    return user.is_authenticated and hasattr(user, 'profile') and user.profile.role == 'Admin'
+def is_librarian(user):
+    return user.is_authenticated and hasattr(user, 'profile') and user.profile.role == 'Librarian'
+def is_member(user):
+    return user.is_authenticated and hasattr(user, 'profile') and user.profile.role == 'Member'
+
+@login_required
+@user_passes_test(is_admin)
+def admin_view(request):
+    return render(request, 'admin/dashboard.html')
+
+@login_required
+@user_passes_test(is_librarian)
+def librarian_view(request):
+    return render(request, 'librarian/dashboard.html')
+
+@login_required
+@user_passes_test(is_member)
+def member_view(request):
+    return render(request, 'member/dashboard.html')
+
