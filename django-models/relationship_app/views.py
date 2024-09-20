997e8174cc940ad908db15a844540efeade8f74d
qayiya tshotsho <mr.qtshotsho@gmail.com>
2024-09-20 19:33:46 +0200

    User restrictions fix

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -3,10 +3,10 @@ from django.shortcuts import render
 from .models import Book
 from .models import Library
 from django.views.generic.detail import DetailView
-#from django.views.generic import DetailView
+from django.views.generic import DetailView
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.decorators import user_passes_test, login_required
+from django.contrib.auth.decorators import user_passes_test
 
 
 
@@ -31,38 +31,3 @@ def register(request):
     else:
         form = UserCreationForm()
     return render(request, 'registration_app/register.html', {'form': form})
-
-@login_required
-@user_passes_test(is_admin)
-
-
-def is_admin(user):
-    return user.is_authenticated and hasattr(user, 'profile') and user.profile.role == 'Admin'
-
-# @login_required
-# @user_passes_test(is_admin)
-def admin_view(request):
-    return render(request, 'admin/dashboard.html')
-
-
-@login_required
-@user_passes_test(is_librarian)
-def is_librarian(request):
-    return user.is_authenticated and hasattr(user, 'profile') and user.profile.role == 'Librarian'
-
-# @login_required
-# @user_passes_test(is_librarian)
-def librarian_view(request):
-    return render(request, 'librarian/dashboard.html')
-
-
-@login_required
-@user_passes_test(is_member)
-def is_member(request):
-    return user.is_authenticated and hasattr(user, 'profile') and user.profile.role = 'Member'
-
-# @login_required
-# @user_passes_test(is_member)
-def member_view(request):
-    return render(request, 'member/dashboard.html')
-
