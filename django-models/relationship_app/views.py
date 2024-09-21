d8e97cd644b341f13a5db19493fe1377c57347ae
qayiya tshotsho <mr.qtshotsho@gmail.com>
2024-09-21 11:14:16 +0200

    User restrictions fix

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -32,12 +32,12 @@ def register(request):
         form = UserCreationForm()
     return render(request, 'relationship_app/register.html', {'form': form})
 
-def is_admin(user):
-    return user.is_authenticated and hasattr(user, 'profile') and user.profile.role == 'Admin'
-def is_librarian(user):
-    return user.is_authenticated and hasattr(user, 'profile') and user.profile.role == 'Librarian'
-def is_member(user):
-    return user.is_authenticated and hasattr(user, 'profile') and user.profile.role == 'Member'
+# def is_admin(user):
+#     return user.is_authenticated and hasattr(user, 'profile') and user.profile.role == 'Admin'
+# def is_librarian(user):
+#     return user.is_authenticated and hasattr(user, 'profile') and user.profile.role == 'Librarian'
+# def is_member(user):
+#     return user.is_authenticated and hasattr(user, 'profile') and user.profile.role == 'Member'
 
 @login_required
 @user_passes_test(is_admin)
