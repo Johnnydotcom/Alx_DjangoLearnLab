c0a648f973746cc91568c75901b13f97e84ef154
qayiya tshotsho <mr.qtshotsho@gmail.com>
2024-09-27 00:29:34 +0200

    access restrictions fi to urls.py

--- django-models/relationship_app/models.py
+++ django-models/relationship_app/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 # Create your models here.
 class Author(models.Model):
@@ -17,21 +20,25 @@ class Library(models.Model):
     name = models.CharField(max_length=100)
     books = models.ManyToManyField(Book)
     
-class Librarian(models.Model):
-    name = models.CharField(max_length=100)
-    library = models.OneToOneField(Library, on_delete=models.CASCADE)
-    
-class Admin(models.Model):
-    name = models.CharField(max_length=100)
-    admin = models.OneToOneField('self', on_delete=models.CASCADE)
-    
-    def __str__(self):
-        return self.name
-    
-class Member(models.Model):
-    name = models.CharField(max_length=100)
-    member = models.OneToOneField('self', on_delete=models.CASCADE)
+class UserProfile(models.Model):
+    ROLE_CHOICES = [
+        ('admin', 'Admin')
+        ('librarian', 'Librarian'),
+        ('member', 'Member'),
+    ]
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    role = models.CharField(max_length=10, choices=ROLE_CHOICES)
     
     def __str__(self):
-        return self.name
+        return f"{self.user.username} - {self.get_role_display()}"
+    
+    @receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        UserProfile.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.userprofile.save()
+
     
