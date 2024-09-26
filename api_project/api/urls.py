be6ec557932f762f2db678498292cbbaeb7170d6
qayiya tshotsho <mr.qtshotsho@gmail.com>
2024-09-26 12:52:10 +0200

    intro to APIs

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 from .views import BookList
 from . import views
+rom rest_framework.routers import DefaultRouter
+
+router.register(r'books_all', BookViewSet, basename='book_all')
 
 urlpatterns = [
-    path('books/', BookList.as_view(), name='book-list'),  
+    path('books/', BookList.as_view(), name='book-list'),
+    path('', include(router.urls)), 
 ]
--- api_project/api/views.py
+++ api_project/api/views.py
@@ -7,3 +7,5 @@ from .serializers import BookSerializer
 class BookList(rest_framework.generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer'
+    
+class BookViewSet(rest_framework.viewsets.ModelViewSet):
