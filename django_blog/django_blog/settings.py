de74d71ded57460d3e12c65bf82d11bc12bb4d60
qayiya tshotsho <mr.qtshotsho@gmail.com>
2024-09-26 14:57:07 +0200

    Building app

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -65,6 +65,7 @@ TEMPLATES = [
                 'django.contrib.messages.context_processors.messages',
             ],
         },
+        'DIRS': [BASE_DIR / 'templates'],
     },
 ]
 
@@ -120,7 +121,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
