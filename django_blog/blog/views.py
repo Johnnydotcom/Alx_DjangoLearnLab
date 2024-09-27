bc447efd98b2ac1db04ee42cf49542fd42c6e244
qayiya tshotsho <mr.qtshotsho@gmail.com>
2024-09-27 09:11:28 +0200

    building a complete Django app

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -8,7 +8,15 @@ def logout_view(request):
     return render(request, 'logout.html')
 
 def register_view(request):
-    return render(request, 'register.html')
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('blog/register.html') 
+    else:
+        form = UserCreationForm()
+    return render(request, 'blog/register.html', {'form': form})
 
 def profile_view(request):
     return render(request, 'profile.html')
