baf78ebac7bd5d8a42230c8cc7b72f5bda652465
qayiya tshotsho <mr.qtshotsho@gmail.com>
2024-09-27 09:00:34 +0200

    building a complete Django app

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('login/', views.hello_view, name='login'),
-    path('logout/', views.AboutView.as_view(), name='logout'),
-    path('register/', views.hello_view, name='register'),
-    path('profile/', views.AboutView.as_view(), name='profile'),
+    path('login/', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout'),
+    path('register/', views.register_view, name='register'),
+    path('profile/', views.profile_view(), name='profile'),
 ]
