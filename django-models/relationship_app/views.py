206c3982e9d10ce028316372bec44544ba7d6fae
qayiya tshotsho <mr.qtshotsho@gmail.com>
2024-09-27 04:58:48 +0200

    access restrictions fi to urls.py

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -7,8 +7,9 @@ from django.views.generic.detail import DetailView
 from django.views.generic import DetailView
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.decorators import user_passes_test, login_required, permission_required
-
+from django.contrib.auth.decorators import user_passes_test
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.decorators import permission_required
 
 
 # Create your views here.
