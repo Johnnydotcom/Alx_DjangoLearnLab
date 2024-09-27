d1cd5b25bc343afa21a00b3a84ba07529c4e0637
qayiya tshotsho <mr.qtshotsho@gmail.com>
2024-09-27 04:36:19 +0200

    access restrictions fi to urls.py

--- django-models/relationship_app/models.py
+++ django-models/relationship_app/models.py
@@ -15,6 +15,12 @@ class Author(models.Model):
 class Book(models.Model):
     title = models.CharField(max_length=100)
     author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='title')
+    class Meta(models.Model):
+        permissions = [
+            ('can_add_book', 'Can add book'),
+            ('can_change_book', 'Can change book'),
+            ('can_delete_book', 'Can delete book'),
+        ]
     
 class Library(models.Model):
     name = models.CharField(max_length=100)
@@ -33,12 +39,12 @@ class UserProfile(models.Model):
         return f"{self.user.username} - {self.get_role_display()}"
     
     @receiver(post_save, sender=User)
-def create_user_profile(sender, instance, created, **kwargs):
+    def create_user_profile(sender, instance, created, **kwargs):
     if created:
         UserProfile.objects.create(user=instance)
 
-@receiver(post_save, sender=User)
-def save_user_profile(sender, instance, **kwargs):
+    @receiver(post_save, sender=User)
+    def save_user_profile(sender, instance, **kwargs):
     instance.userprofile.save()
 
     
