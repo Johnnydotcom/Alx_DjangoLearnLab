591d9605fa5423ad51c1c35010faba8047454e12
qayiya tshotsho <mr.qtshotsho@gmail.com>
2024-09-27 05:03:00 +0200

    urls,py update path to add, change or delete book.

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -16,4 +16,7 @@ urlpatterns = [
     path('admin/', admin_view, name='admin_view'),
     path('librarian/', librarian_view, name='librarian_view'),
     path('member/', member_view, name='member_view'),
+    path('books/add/', add_book, name='add_book'),
+    path('books/change/', change_book, name='change_book'),
+    path('books/delete/', delete_book, name='delete_book'),
 ]
