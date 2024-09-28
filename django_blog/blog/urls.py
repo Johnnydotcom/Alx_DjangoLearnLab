7e6cf0d42f6ef0d20c44b1847f626418df3f01c9
qayiya tshotsho <mr.qtshotsho@gmail.com>
2024-09-28 09:52:09 +0200

    building a complete Django app

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -6,4 +6,9 @@ urlpatterns = [
     path('logout/', views.logout_view, name='logout'),
     path('register/', views.register_view, name='register'),
     path('profile/', views.profile_view(), name='profile'),
+    path('blog', PostListView.as_view(), name='list'),
+    path('blog', PostDetailView.as_view(), name='detail'),
+    path('blog', PostCreateView.as_view(), name='create'),
+    path('blog', PostUpdateView.as_view(), name='update'),
+    path('blog/', PostDeleteView.as_view(), name='delete')
 ]
--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views.generic import DeleteView, DetailView, CreateView, UpdateView
 
 # Create your views here.
 def login_view(request):
@@ -20,3 +21,23 @@ def register_view(request):
 
 def profile_view(request):
     return render(request, 'profile.html')
+
+class PostUpdateView(UpdateView):
+    model = Post
+    template_name = 'update.html' 
+    fields = ['title', 'content']
+    
+class PostCreateView(CreateView):
+    model = Post
+    template_name = 'create.html'   # Template for creating a post
+    fields = ['title', 'content']
+    
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'detail.html'  # Template for a single post
+    context_object_name = 'post'
+    
+class PostDeleteView(DeleteView):
+    model = Post
+    template_name = 'delete.html'  # Template for delete confirmation
+    success_url = reverse_lazy('post-list')
