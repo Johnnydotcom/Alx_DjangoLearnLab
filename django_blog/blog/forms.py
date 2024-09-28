33486fa338daa3a8e1269a85c78a5be197366054
qayiya tshotsho <mr.qtshotsho@gmail.com>
2024-09-28 10:14:00 +0200

    building a complete Django app

---
+++ django_blog/blog/forms.py
@@ -0,0 +1,12 @@
+from django import forms
+from .models import Post
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ['title', 'content']
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['title'].label = "Post Title"
+        self.fields['content'].label = "Post Content"
